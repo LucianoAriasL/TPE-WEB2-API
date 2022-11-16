ab6f88888bc472757384508c6f280edf336257e0
LucianoAriasL <110885538+LucianoAriasL@users.noreply.github.com>
2022-11-15 22:55:40 -0300

    Update ZapatillasApiView.php

--- app/views/ZapatillasApiView.php
+++ app/views/ZapatillasApiView.php
@@ -4,7 +4,6 @@ class ZapatillasApiView{
     header("Content-Type: application/json");
     header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
 
-    // convierte los datos a un formato json
     echo json_encode($data);
   }
 
